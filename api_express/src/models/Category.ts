d88af9aaf1c01bad84704f4f1e570879eb43c465
erdemcasa <pl23ksc@eduvaud.ch>
2026-04-29 08:39:07 +0200

    Ajout du model Category

---
+++ api_express/src/models/Category.ts
@@ -0,0 +1,42 @@
+import {
+  CreationOptional,
+  InferAttributes,
+  InferCreationAttributes,
+} from "sequelize";
+import {
+  AllowNull,
+  Column,
+  CreatedAt,
+  DataType,
+  Model,
+  Table,
+  Unique,
+  UpdatedAt,
+} from "sequelize-typescript";
+
+@Table({
+  tableName: "categories",
+  modelName: "Category",
+})
+export default class User extends Model<
+  InferAttributes<User>,
+  InferCreationAttributes<User>
+> {
+  @Column({
+    primaryKey: true,
+    type: DataType.BIGINT,
+    autoIncrement: true,
+  })
+  declare id: CreationOptional<number>;
+
+  @AllowNull(false)
+  @Unique
+  @Column
+  declare name: string;
+
+  @CreatedAt
+  declare created_at: CreationOptional<Date>;
+
+  @UpdatedAt
+  declare updated_at: CreationOptional<Date>;
+}
