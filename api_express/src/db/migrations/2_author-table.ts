4c0c0a35147b71ff8e7073c68409e49df2f711c8
Zidane Sahraoui <pd01wvj@eduvaud.ch>
2026-04-29 08:51:33 +0200

    feat(migration): réalisation de author_table

--- api_express/src/db/migrations/2_author-table.ts
+++ api_express/src/db/migrations/2_author-table.ts
@@ -1,28 +1,37 @@
-import { DataTypes, Sequelize } from "sequelize";
-import type { Migration } from "../../umzug";
+import { DataTypes, Sequelize } from 'sequelize';
+import type { Migration } from '../../umzug';
 
 export const up: Migration = async ({ context: sequelize }) => {
-  await sequelize.getQueryInterface().createTable("users", {
+  await sequelize.getQueryInterface().createTable('users', {
     id: {
       type: DataTypes.BIGINT,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
     },
-    
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: false,
+    },
     created_at: {
       type: DataTypes.DATE,
       allowNull: false,
-      defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
     },
     updated_at: {
       type: DataTypes.DATE,
       allowNull: false,
-      defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
     },
   });
 };
 
 export const down: Migration = async ({ context: sequelize }) => {
-  await sequelize.getQueryInterface().dropTable("users");
+  await sequelize.getQueryInterface().dropTable('users');
 };
