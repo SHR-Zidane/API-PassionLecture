86fbb3a7e05539f41735447d52c43810aaf80635
Zidane Sahraoui <pd01wvj@eduvaud.ch>
2026-04-29 09:24:28 +0200

    fix(models): correction @ForeignKey(() => User) => @ForeignKey(() => Book)

--- api_express/src/models/Review.ts
+++ api_express/src/models/Review.ts
@@ -62,6 +62,6 @@ export default class Review extends Model<
   @Column({ type: DataType.BIGINT })
   declare book_id: number;
 
-  @BelongsTo(() => User)
+  @BelongsTo(() => Book)
   declare book: Book;
 }
